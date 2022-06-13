91bc92ce22a7e8501c9d70e9150ffa831f92c221
habybikhsan <habybikhsan@gmail.com>
2022-06-14 00:22:11 +0700

    new

---
+++ app/src/main/java/com/example/mystoryapp/api/ApiService.kt
@@ -0,0 +1,29 @@
+package com.c22_ce02.awmonitorapp.api
+
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.Call
+import retrofit2.http.*
+
+interface ApiService {
+    @Headers("Content-Type: application/json")
+    @POST("register")
+    fun createUser(@Body requestRegister: RequestRegister): Call<ResponseMsg>
+
+    @POST("login")
+    fun fetchUser(@Body requestLogin: RequestLogin): Call<ResponseLogin>
+
+    @GET("stories")
+    fun getStories(
+        @Header("Authorization") token: String
+    ): Call<ResponseStory>
+
+    @Multipart
+    @POST("stories")
+    fun uploadImage(
+        @Part file: MultipartBody.Part,
+        @Part("description") description: RequestBody,
+        @Header("Authorization") token: String
+    ): Call<ResponseMsg>
+    
+}
